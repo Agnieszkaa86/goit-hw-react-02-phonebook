e6a70bf5ff3cfaa86f9b806040f47544102d4f24
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-12-28 11:43:09 +0100

    fix export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import {Component} from "react";
 import { nanoid } from "nanoid";
 import ContactForm  from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
-import { ContactList } from "ContactList/ContactList";
+import { ContactList } from "components/ContactItem/ContactList/ContactList";
 import { Container } from './ContactForm/ContactForm.styled'
 
 export const INITIAL_STATE = {
